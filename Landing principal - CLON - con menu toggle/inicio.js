f24c09f76a005ae2296616ef19d266c277caa23a
tiagodevv1-crypto <tiago.dev.v1@gmail.com>
2026-02-13 10:03:35 -0300

    Delete Landing principal - CLON - con menu toggle directory

--- Landing principal - CLON - con menu toggle/inicio.js
+++
@@ -1,9 +0,0 @@
-// inicio.js
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.querySelector('.menu-toggle');
-  const nav = document.querySelector('.nav-inicio');
-
-  toggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-  });
-});
